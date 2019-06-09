ade265eaa7c472e46935aba90921bebd633960ac
hiddevuijk <hdvuijk@gmail.com>
2019-06-09 18:13:32 +0200

    stuff

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-import tkinter
+""" tkintert program """
+import tkinter as tk
 
+def print_name(event):
+    """ prints name """
+    print("My name is Hidde.")
+
+
+def main():
+    """ main loop """
+
+    root = tk.Tk()
+
+    button_1 = tk.Button(root, text="Print Name")
+    button_1.bind("<Button-1>", print_name)
+    button_1.pack()
+
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
