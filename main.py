2e953750ca688748b5f2d280630486ee3819a30c
hiddevuijk <hdvuijk@gmail.com>
2019-06-09 21:49:50 +0200

    d

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 """ tkintert program """
 import tkinter as tk
-
-def do_nothing():
-    """ do nothing """
-    print(" I've done nothing. ")
-
+from tkinter import messagebox
 
 def main():
     """ main loop """
 
     root = tk.Tk()
 
-    menu = tk.Menu(root)
-    root.config(menu=menu)
-
-    sub_menu = tk.Menu(menu)
-    menu.add_cascade(label="File", menu=sub_menu)
-    sub_menu.add_command(label="New Project ...", command=do_nothing)
-    sub_menu.add_command(label="New ...", command=do_nothing)
-    sub_menu.add_separator()
-    sub_menu.add_command(label="Exit", command=do_nothing)
-
-    edit_menu = tk.Menu(menu)
-    menu.add_cascade(label="Edit", menu=edit_menu)
-    edit_menu.add_command(label="Redo", command=do_nothing)
+    answer = messagebox.askquestion("Question 1", "Do you like this?")
+    if answer == 'yes':
+        print(10_00)
 
     root.mainloop()
 
