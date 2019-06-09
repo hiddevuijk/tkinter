8a4b47677d6b37400f06f9bba75917c86dc25bbd
hiddevuijk <hdvuijk@gmail.com>
2019-06-09 19:47:13 +0200

    ; "

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """ tkintert program """
 import tkinter as tk
 
-def print_name(event):
-    """ prints name """
-    print("My name is Hidde.")
+def do_nothing():
+    """ do nothing """
+    print(" I've done nothing. ")
 
 
 def main():
@@ -11,10 +11,19 @@ def main():
 
     root = tk.Tk()
 
-    button_1 = tk.Button(root, text="Print Name")
-    button_1.bind("<Button-1>", print_name)
-    button_1.pack()
+    menu = tk.Menu(root)
+    root.config(menu=menu)
 
+    sub_menu = tk.Menu(menu)
+    menu.add_cascade(label="File", menu=sub_menu)
+    sub_menu.add_command(label="New Project ...", command=do_nothing)
+    sub_menu.add_command(label="New ...", command=do_nothing)
+    sub_menu.add_separator()
+    sub_menu.add_command(label="Exit", command=do_nothing)
+
+    edit_menu = tk.Menu(menu)
+    menu.add_cascade(label="Edit", menu=edit_menu)
+    edit_menu.add_command(label="Redo", command=do_nothing)
 
     root.mainloop()
 
